83967997fab784a0da0a5b60782a9c1a9a0ea2ea
Akif Ullah <akifullah0317@gmail.com>
2025-07-27 20:08:24 +0500

    Enhance PasswordProtectMiddleware to exclude specific routes from authentication checks

--- app/Http/Middleware/PasswordProtectMiddleware.php
+++ app/Http/Middleware/PasswordProtectMiddleware.php
@@ -10,16 +10,23 @@ class PasswordProtectMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
+
+        // Define excluded route names
+        $excludedRouteNames = ['canva.download', 'canva.preview'];
+
+        if ($request->route() && in_array($request->route()->getName(), $excludedRouteNames)) {
+            return $next($request);
+        }
+
         // Allow access to password form and submission
         if ($request->is('password') || $request->is('password/submit')) {
             return $next($request);
         }
 
-        // Check if the user is authenticated for the site
         if (!Session::get('site_authenticated')) {
             return redirect('/password');
         }
-        // Check for expiration
+
         $expiresAt = Session::get('site_authenticated_expires_at');
         if (!$expiresAt || $expiresAt < time()) {
             Session::forget(['site_authenticated', 'site_authenticated_expires_at']);
@@ -27,5 +34,23 @@ class PasswordProtectMiddleware
         }
 
         return $next($request);
+
+        // // Allow access to password form and submission
+        // if ($request->is('password') || $request->is('password/submit')) {
+        //     return $next($request);
+        // }
+
+        // // Check if the user is authenticated for the site
+        // if (!Session::get('site_authenticated')) {
+        //     return redirect('/password');
+        // }
+        // // Check for expiration
+        // $expiresAt = Session::get('site_authenticated_expires_at');
+        // if (!$expiresAt || $expiresAt < time()) {
+        //     Session::forget(['site_authenticated', 'site_authenticated_expires_at']);
+        //     return redirect('/password');
+        // }
+
+        // return $next($request);
     }
-} 
+}
